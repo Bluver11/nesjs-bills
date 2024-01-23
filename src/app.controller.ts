671c806d9078befa9ea4fa286da4b9c6fe652643
Petrik Diak <diak@petrik.hu>
2024-01-23 15:28:18 +0100

    Feladat megalkotása

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,77 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly prismaService: PrismaService
+  constructor(private readonly appService: AppService,
+    prismaService:PrismaService
+    ) {
+      this.prismaService = prismaService;
+    }
 
   @Get()
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
   }
+
+
+  @Get('bills')
+  listBills(){
+    return this.prismaService.bills.findMany();
+  }
+
+
+  @Get('bills//id/:id')
+  async oneBill(@Param('id') id: string){
+    try{
+      return await this.prismaService.bills.findFirstOrThrow({
+        where:{
+          id: parseInt(id)
+        }
+      })
+    }catch{
+      throw new NotFoundException('Nincs ilyen ID!')
+    }
+  }
+
+  @Get('bills/type/:type')
+  async oneType(@Param('type') type: string){
+    try{
+      return await this.prismaService.bills.findMany({
+        where:{
+          type: type
+        }
+      })
+    }catch{
+      throw new NotFoundException('Nincs ilyen számla')
+    }
+  }
+
+
+  @Get('bills/urgent')
+  async urgentList(){
+    return await this.prismaService.bills.findFirst({
+      orderBy:{
+        due: 'asc'
+      }
+    })
+  }
+
+  @Get('bills/late')
+  async lateList(){
+    return await this.prismaService.bills.findMany({
+      where: {
+        due:{
+          lt: new Date()
+        }
+        
+      }
+      
+    })
+  }
+
+
 }
